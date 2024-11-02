64abd63d4273f274779b2a76598b24a0a45848c4
UltimateQuack <patoflamejantetv@gmail.com>
2024-11-02 18:34:40 -0300

    Tô com vergonha... - ver-goinhaa~
    sou muito burro scrr

--- shrekSendScript.js
+++ shrekSendScript.js
@@ -1,26 +1,26 @@
+let COPYPASTA = prompt("Insira o texto que quer enviar:");
 async function enviarScript(scriptText){
-	COPYPASTA = prompt("Insira o texto que quer enviar:")
-	const lines = scriptText.split(/[\n\t]+/).map(line => line.trim()).filter(line => line);
-	main = document.querySelector("#main"),
-	textarea = main.querySelector(`div[contenteditable="true"]`)
-	
-	if(!textarea) throw new Error("Não há uma conversa aberta")
-	
-	for(const line of lines){
-		console.log(line)
-	
-		textarea.focus();
-		document.execCommand('insertText', false, line);
-		textarea.dispatchEvent(new Event('change', {bubbles: true}));
-	
-		setTimeout(() => {
-			(main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click();
-		}, 100);
-		
-		if(lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 250));
-	}
-	
-	return lines.length;
+    const lines = scriptText.split(/[\n\t]+/).map(line => line.trim()).filter(line => line);
+    main = document.querySelector("#main"),
+    textarea = main.querySelector(`div[contenteditable="true"]`)
+    
+    if(!textarea) throw new Error("Não há uma conversa aberta")
+    
+    for(const line of lines){
+        console.log(line)
+    
+        textarea.focus();
+        document.execCommand('insertText', false, line);
+        textarea.dispatchEvent(new Event('change', {bubbles: true}));
+    
+        setTimeout(() => {
+            (main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click();
+        }, 100);
+        
+        if(lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 250));
+    }
+    
+    return lines.length;
 }
 
 enviarScript(COPYPASTA).then(e => console.log(`Código finalizado, ${e} mensagens enviadas`)).catch(console.error)
